9ac2924ac6cbaea7cd7526de013dd94909893ef3
Harsh Chinchkhede <harshchinchkhede1.sit.it@gmail.com>
2026-09-22 18:07:40 +0530

    Change

--- hello-world/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Typescript  built on top of javascript,typescript is superset of javascript has everything feature that has javascript..
-//Any javascript code valid typescript code..
-let age = 20;
