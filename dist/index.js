663b5cc2d55d6de9fd2fe052d66310f4aabee6bd
Harsh Chinchkhede <harshchinchkhede1.sit.it@gmail.com>
2026-09-22 18:20:44 +0530

    Configure

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Typescript  built on top of javascript,typescript is superset of javascript has everything feature that has javascript..
+//Any javascript code valid typescript code..
+let age = 20;
+//# sourceMappingURL=index.js.map
