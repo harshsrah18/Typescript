6921e8827adb86128ec6e90ad65a75299ccd03ff
Harsh Chinchkhede <harshchinchkhede1.sit.it@gmail.com>
2026-09-22 18:27:17 +0530

    Configure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 //Typescript  built on top of javascript,typescript is superset of javascript has everything feature that has javascript..
 //Any javascript code valid typescript code..
 let age: number = 20;
+
